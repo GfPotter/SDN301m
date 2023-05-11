cbc80f5ca1f5062a879020c5f6096b182a70a70d
GfPotter <quanpotterha@gmail.com>
2023-05-11 08:57:28 +0700

    first update

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var rec = {
-    perimeter: (x, y) => (2 * (x + y)),
-    area: (x, y) => (x * y)
-}
+var rec = require('./rectangle');
 
 var solveRect = (l, b) => {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
